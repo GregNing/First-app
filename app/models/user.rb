952cb7e95e0d3a8430d3d8f886bf69d70058bf6f
Greg <GregJieNing@gmail.com>
2018-01-15 21:17:48 +0800

    add user_id

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+  #連結user 與  group 雙向關西
+  has_many :groups
+end
---
+++ db/migrate/20180115131316_add_user_id_to_group.rb
@@ -0,0 +1,4 @@
+class AddUserIdToGroup < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
