950c79aa9a80a16567727a8f8cb85868485ace54
Greg <GregJieNing@gmail.com>
2018-01-20 20:44:44 +0800

    join quit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join , :quit]
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     def index
         @groups = Group.all
@@ -52,6 +52,29 @@ class GroupsController < ApplicationController
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
+    end
+
+    def join
+        @group = Group.find(params[:id])
+
+        if !current_user.is_member_of?(@group)
+            current_user.join!(@group)
+            flash[:notice] = "加入 #{@group.title} 成功!" 
+        else
+            flash[:warning] = "你已經是 #{@group.title} 成員了！"
+        end
+        redirect_to group_path(@group)
+    end
+    def quit
+        @group = Group.find(params[:id])
+
+        if current_user.is_member_of?(@group)
+            current_user.quit!(@group)
+            flash[:alert] = "已退出 #{@group.title} 成員!"
+        else
+            flash[:warning] = "你不是 #{@group.title} 成員"
+        end        
+        redirect_to group_path(@group)
     end
     private
     
